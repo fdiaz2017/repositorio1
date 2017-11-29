5f8bb762003db5a77fba7247d6702a13a57c79ba
LABS\iocp <federico.diaz95@outlook.com>
2017-11-29 18:15:20 -0300

    mensaje

---
+++ proyecto/pagina.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class pagina here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pagina
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class pagina
+     */
+    public pagina()
+    {
+        System.out.print("Probando Git");
+    }
+}
